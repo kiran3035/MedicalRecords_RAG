4a1a216e5956a457617f213da87e4e286cc84b0c
kiran3035 <67626889+kiran3035@users.noreply.github.com>
2025-09-28 18:42:58 -0600

    Folder Added Succesfully

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creating Directories
+mkdir -p Sources
+mkdir -p Research
+
+#Creating Files
+touch Sources/ __init__.py
+touch Research/ helper.py
+touch Research/ prompt.py
+touch .env
+touch setup.py
+touch Applications.py
+touch research/Trials.ipynb
+touch Requirements.txt
+
+
+echo "Directories and Files created successfully."
